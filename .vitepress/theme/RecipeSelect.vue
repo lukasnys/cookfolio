53be22e0eb9ad89536f73ea91292d17029092cf9
lukasnys <nyslukas@gmail.com>
2025-08-10 18:54:46 +0200

    feat: hook up recipe selection logic

--- .vitepress/theme/RecipeSelect.vue
+++ .vitepress/theme/RecipeSelect.vue
@@ -1,9 +1,22 @@
 <script setup>
   import { data as recipes } from './recipes.data.ts';
+
+  defineProps({
+    selectedRecipe: String,
+    dayId: String
+  });
+
+  const emit = defineEmits(['update:recipe']);
 </script>
 
 <template>
-  <select id="monday" name="monday" class="recipe-select">
+  <select 
+    :id="dayId" 
+    :name="dayId" 
+    class="recipe-select"
+    :value="selectedRecipe"
+    @change="emit('update:recipe', $event.target.value)"
+  >
     <option value="">Select a recipe</option>
     <option v-for="recipe in recipes" :value="recipe.title">{{ recipe.title }}</option>
   </select>
--- .vitepress/theme/WeekPlanner.vue
+++ .vitepress/theme/WeekPlanner.vue
@@ -1,5 +1,19 @@
 <script setup>
 import RecipeSelect from './RecipeSelect.vue';
+
+const weekData = [
+  { id: 'monday', label: 'Monday', recipe: null },
+  { id: 'tuesday', label: 'Tuesday', recipe: null },
+  { id: 'wednesday', label: 'Wednesday', recipe: null },
+  { id: 'thursday', label: 'Thursday', recipe: null },
+  { id: 'friday', label: 'Friday', recipe: null },
+  { id: 'saturday', label: 'Saturday', recipe: null },
+  { id: 'sunday', label: 'Sunday', recipe: null }
+];
+
+const saveWeek = () => {
+  console.log(weekData);
+}
 </script>
 
 <style scoped>
@@ -33,41 +47,15 @@ import RecipeSelect from './RecipeSelect.vue';
   <h2>Week Planner</h2>
 
   <div class="week-planner">
-    <div class="day-field">
-      <label for="monday">Monday</label>
-      <RecipeSelect />
-    </div>
-    
-    <div class="day-field">
-      <label for="tuesday">Tuesday</label>
-      <RecipeSelect />
-    </div>
-    
-    <div class="day-field">
-      <label for="wednesday">Wednesday</label>
-      <RecipeSelect />
-    </div>
-    
-    <div class="day-field">
-      <label for="thursday">Thursday</label>
-      <RecipeSelect />
-    </div>
-    
-    <div class="day-field">
-      <label for="friday">Friday</label>
-      <RecipeSelect />
-    </div>
-    
-    <div class="day-field">
-      <label for="saturday">Saturday</label>
-      <RecipeSelect />
-    </div>
-    
-    <div class="day-field">
-      <label for="sunday">Sunday</label>
-      <RecipeSelect />
+    <div v-for="(day, index) in weekData" :key="day.id" class="day-field">
+      <label :for="day.id">{{ day.label }}</label>
+      <RecipeSelect 
+        :selectedRecipe="day.recipe"
+        :dayId="day.id"
+        @update:recipe="value => weekData[index].recipe = value"
+      />
     </div>
 
-    <button class="btn btn-brand">Save</button>
+    <button class="btn btn-brand" @click="saveWeek">Save</button>
   </div>
 </template>
