72f37cbc2e2d322b702bced953c047f0d369f928
lukasnys <nyslukas@gmail.com>
2025-08-11 08:45:00 +0200

    feat: add types and add ingredients in recipes

--- .vitepress/theme/recipes.data.ts
+++ .vitepress/theme/recipes.data.ts
@@ -1,10 +1,26 @@
 import { createContentLoader } from 'vitepress'
 
+export interface Ingredient {
+  name: string;
+  quantity: number;
+  unit: string;
+}
+
+export interface Recipe {
+  title: string;
+  url: string;
+  ingredients: Ingredient[];
+}
+
+declare const data: Recipe[];
+export { data };
+
 export default createContentLoader('recipes/*.md', {
   transform: (content) => {
     return content.map((page) => ({
       title: page.frontmatter.title,
       url: page.url.replace('/', ''),
+      ingredients: page.frontmatter.ingredients,
     }));
   }
 })
